0d152636e1450d5b2158f70e4019e35cd56aeef3
Dan Gunter <dkgunter@lbl.gov>
2017-02-10 08:15:28 -0800

    Support for animated gif captioning

---
+++ pycaptioner/anim.py
@@ -0,0 +1,31 @@
+"""
+Functions for animated images.
+"""
+from PIL import Image
+import numpy
+
+def process_frames(img, func):
+    """Process an animated image, applying func() to each frame.
+
+    Args:
+        img (ImageSequence): Animated image (i.e. GIF)
+        func: Function taking an Image and returning an Image
+
+    Returns:
+        (list) List of numpy arrays that can be saved by imageio.mimsave()
+
+    """
+    sequence, frame_num = [], 0
+    while True:
+        try:
+            img.seek(frame_num)
+        except EOFError:
+            break
+        new_img = Image.new("RGB", img.size)
+        new_img.paste(img)
+        mod_img = func(new_img)
+        np_img = numpy.array(mod_img)
+        sequence.append(np_img)
+        frame_num += 1
+    return sequence
+
--- pycaptioner/commands.py
+++ pycaptioner/commands.py
@@ -4,9 +4,10 @@ Command-line program functions
 import argparse
 import sys
 
-from . import addtext
 from PIL import Image
+from imageio import mimsave
 
+from . import addtext, anim
 
 class Caption(object):
     sides = {'t': addtext.Side.TOP, 'b': addtext.Side.BOTTOM,
@@ -18,12 +19,13 @@ class Caption(object):
         self.p.add_argument('inp', help='Input image file')
         self.p.add_argument('out', help='Output image file')
         self.p.add_argument('txt', help='Caption text (use \'-\' for standard input)')
-        self.p.add_argument('--vpad', dest='padv', type=int, default=default_pad,
-                            metavar='PX',
-                            help='Vertical padding, in pixels (default=%(default)s)')
+        self.p.add_argument('-a', '--anim', help='Animated image', action='store_true', dest='anim')
         self.p.add_argument('--hpad', dest='padh', type=int, default=default_pad,
                             metavar='PX',
                             help='Horizontal padding, in pixels (default=%(default)s)')
+        self.p.add_argument('--vpad', dest='padv', type=int, default=default_pad,
+                            metavar='PX',
+                            help='Vertical padding, in pixels (default=%(default)s)')
         self.p.add_argument('-r', '--reverse', dest='rev', action='store_true',
                             help='White text on black background')
         self.p.add_argument('-s', '--side', dest='side', default='b',
@@ -52,12 +54,23 @@ class Caption(object):
             text = sys.stdin.read()
         else:
             text = a.txt
-        caption_one(a.inp, text, a.out, opts)
-
+        if anim:
+            caption_sequence(a.inp, text, a.out, opts)
+        else:
+            caption_one(a.inp, text, a.out, opts)
 
 def caption_one(input_path, caption_text, output_path, options):
     image = Image.open(input_path)
+    output_image = _add_caption(image, caption_text, options)
+    output_image.save(output_path)
+
+def caption_sequence(input_path, caption_text, output_path, options):
+    image = Image.open(input_path)
+    sequence = anim.process_frames(image, lambda im: _add_caption(im, caption_text, options))
+    mimsave(output_path, sequence)
+
+def _add_caption(image, caption_text, options):
     cap = addtext.CapImg(image, **options)
     cap.addtext(caption_text)
-    output_image = cap.finish()
-    output_image.save(output_path)
+    return cap.finish()
+
